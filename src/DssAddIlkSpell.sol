c9a75f26bef63e2b84f0f044f85c91c5ceace43f
fabiohild <fabiohildebrand@gmail.com>
2020-08-21 13:42:16 -0300

    WIP bringing in dss-add-illk-spell

---
+++ src/DssAddIlkSpell.sol
@@ -0,0 +1,85 @@
+pragma solidity >=0.6.7;
+
+abstract contract SpotterLike {
+    function poke(bytes32) public virtual;
+}
+
+abstract contract PauseLike {
+    function delay() public virtual returns (uint);
+    function exec(address, bytes32, bytes memory, uint256) public virtual;
+    function plot(address, bytes32, bytes memory, uint256) public virtual;
+}
+
+abstract contract ConfigLike {
+    function init(bytes32) public virtual;
+    function file(bytes32, bytes32, address) public virtual;
+    function file(bytes32, bytes32, uint) public virtual;
+    function rely(address) public virtual;
+}
+
+contract IlkDeployer {
+    function deploy(bytes32 ilk_, address[8] calldata addrs, uint[5] calldata values) external {
+        // addrs[0] = vat
+        // addrs[1] = cat
+        // addrs[2] = jug
+        // addrs[3] = spotter
+        // addrs[4] = end
+        // addrs[5] = join
+        // addrs[6] = pip
+        // addrs[7] = flip
+        // values[0] = line
+        // values[1] = mat
+        // values[2] = duty
+        // values[3] = chop
+        // values[4] = lump
+
+        ConfigLike(addrs[3]).file(ilk_, "pip", address(addrs[6])); // vat.file(ilk_, "pip", pip);
+
+        ConfigLike(addrs[1]).file(ilk_, "flip", addrs[7]); // cat.file(ilk_, "flip", flip);
+        ConfigLike(addrs[0]).init(ilk_); // vat.init(ilk_);
+        ConfigLike(addrs[2]).init(ilk_); // jug.init(ilk_);
+
+        ConfigLike(addrs[0]).rely(addrs[5]); // vat.rely(join);
+        ConfigLike(addrs[7]).rely(addrs[1]); // flip.rely(cat);
+        ConfigLike(addrs[7]).rely(addrs[4]); // flip.rely(end);
+
+        ConfigLike(addrs[0]).file(ilk_, "line", values[0]); // vat.file(ilk_, "line", line);
+        ConfigLike(addrs[1]).file(ilk_, "lump", values[4]); // cat.file(ilk_, "lump", lump);
+        ConfigLike(addrs[1]).file(ilk_, "chop", values[3]); // cat.file(ilk_, "chop", chop);
+        ConfigLike(addrs[2]).file(ilk_, "duty", values[2]); // jug.file(ilk_, "duty", duty);
+        ConfigLike(addrs[3]).file(ilk_, "mat", values[1]); // spotter.file(ilk_, "mat", mat);
+
+        SpotterLike(addrs[3]).poke(ilk_); // spotter.poke(ilk_);
+    }
+}
+
+contract DssAddIlkSpell {
+    bool      public done;
+    address   public pause;
+
+    address   public action;
+    bytes32   public tag;
+    uint256   public eta;
+    bytes     public sig;
+
+    constructor(bytes32 ilk_, address pause_, address[8] memory addrs, uint[5] memory values) public {
+        pause = pause_;
+        address ilkDeployer = address(new IlkDeployer());
+        sig = abi.encodeWithSignature("deploy(bytes32,address[8],uint256[5])", ilk_, addrs, values);
+        bytes32 _tag; assembly { _tag := extcodehash(ilkDeployer) }
+        action = ilkDeployer;
+        tag = _tag;
+    }
+
+    function schedule() external {
+        require(eta == 0, "spell-already-scheduled");
+        eta = now + PauseLike(pause).delay();
+        PauseLike(pause).plot(action, tag, sig, eta);
+    }
+
+    function cast() public {
+        require(!done, "spell-already-cast");
+        done = true;
+        PauseLike(pause).exec(action, tag, sig, eta);
+    }
+}
---
+++ src/test/DssAddIlkSpell.t.sol
@@ -0,0 +1,124 @@
+pragma solidity >=0.6.7;
+
+import "ds-test/test.sol";
+
+import "geb-deploy/test/GebDeploy.t.base.sol";
+
+import "../DssAddIlkSpell.sol";
+
+import {OracleLike} from "geb/OracleRelayer.sol";
+import {BasicCollateralJoin} from "geb/BasicTokenAdapters.sol";
+
+contract DssAddIlkSpellTest is GebDeployTestBase {
+    DssAddIlkSpell spell;
+
+    bytes32 constant ilk = "NCT"; // New Collateral Type
+    DSToken     nct;
+    BasicCollateralJoin     nctJoin;
+    FixedDiscountCollateralAuctionHouse     nctFlip;
+    DSValue     nctPip;
+
+    function setUp() public override {
+        super.setUp();
+        deployStable("");
+
+        nct = new DSToken(ilk);
+        nct.mint(1 ether);
+        nctJoin = new BasicCollateralJoin(address(cdpEngine), ilk, address(nct));
+        nctPip = new DSValue();
+        nctPip.updateResult(uint(300 ether));
+        nctFlip = fixedDiscountCollateralAuctionHouseFactory.newCollateralAuctionHouse(address(cdpEngine), ilk);
+        nctFlip.addAuthorization(address(pause.proxy()));
+        nctFlip.removeAuthorization(address(this));
+
+        spell = new DssAddIlkSpell(
+            ilk,
+            address(pause),
+            [
+                address(cdpEngine),
+                address(liquidationEngine),
+                address(taxCollector),
+                address(oracleRelayer),
+                address(globalSettlement),
+                address(nctJoin),
+                address(nctPip),
+                address(nctFlip)
+            ],
+            [
+                10000 * 10 ** 45, // line
+                1500000000 ether, // mat
+                1.05 * 10 ** 27, // tax
+                ONE, // chop
+                10000 ether // lump
+            ]
+        );
+
+        authority.setRootUser(address(spell), true);
+
+        // spell.schedule(); // failing call
+        // spell.cast();
+
+        nct.approve(address(nctJoin), 1 ether);
+    }
+
+    function testVariables() public {
+        (,,,uint line,,) = cdpEngine.collateralTypes(ilk);
+        assertEq(line, uint(10000 * 10 ** 45));
+        (OracleLike pip, uint mat,) = oracleRelayer.collateralTypes(ilk);
+        assertEq(address(pip), address(nctPip));
+        assertEq(mat, uint(1500000000 ether));
+        (uint tax,) = taxCollector.collateralTypes(ilk);
+        assertEq(tax, uint(1.05 * 10 ** 27));
+        (address flip, uint chop, uint lump) = liquidationEngine.collateralTypes(ilk);
+        assertEq(flip, address(nctFlip));
+        assertEq(chop, ONE);
+        assertEq(lump, uint(10000 ether));
+        assertEq(cdpEngine.authorizedAccounts(address(nctJoin)), 1);
+    }
+
+    function testFrob() public {
+        assertEq(coin.balanceOf(address(this)), 0);
+        nctJoin.join(address(this), 1 ether);
+
+        cdpEngine.modifyCDPCollateralization(ilk, address(this), address(this), address(this), 1 ether, 100 ether);
+
+        cdpEngine.approveCDPModification(address(coinJoin));
+        coinJoin.exit(address(this), 100 ether);
+        assertEq(coin.balanceOf(address(this)), 100 ether);
+    }
+
+    function testFlip() public {
+        this.modifyParameters(address(liquidationEngine), ilk, "lump", 1 ether); // 1 unit of collateral per batch
+        this.modifyParameters(address(liquidationEngine), ilk, "chop", ONE);
+        nctJoin.join(address(this), 1 ether);
+        cdpEngine.modifyCDPCollateralization(ilk, address(this), address(this), address(this), 1 ether, 200 ether); // Maximun DAI generated
+        nctPip.updateResult(uint(300 ether - 1)); // Decrease price in 1 wei
+        oracleRelayer.updateCollateralPrice(ilk);
+        assertEq(cdpEngine.tokenCollateral(ilk, address(nctFlip)), 0);
+        uint batchId = liquidationEngine.liquidateCDP(ilk, address(this));
+        assertEq(cdpEngine.tokenCollateral(ilk, address(nctFlip)), 1 ether);
+
+        address(user1).transfer(10 ether);
+        user1.doEthJoin(address(weth), address(ethJoin), address(user1), 10 ether);
+        user1.doModifyCDPCollateralization(address(cdpEngine), "ETH", address(user1), address(user1), address(user1), 10 ether, 1000 ether);
+
+        address(user2).transfer(10 ether);
+        user2.doEthJoin(address(weth), address(ethJoin), address(user2), 10 ether);
+        user2.doModifyCDPCollateralization(address(cdpEngine), "ETH", address(user2), address(user2), address(user2), 10 ether, 1000 ether);
+
+        user1.doCDPApprove(address(cdpEngine), address(nctFlip));
+        user2.doCDPApprove(address(cdpEngine), address(nctFlip));
+
+        user1.doIncreaseBidSize(address(nctFlip), batchId, 1 ether, rad(100 ether));
+        user2.doIncreaseBidSize(address(nctFlip), batchId, 1 ether, rad(140 ether));
+        user1.doIncreaseBidSize(address(nctFlip), batchId, 1 ether, rad(180 ether));
+        user2.doIncreaseBidSize(address(nctFlip), batchId, 1 ether, rad(200 ether));
+
+        user1.doDecreaseSoldAmount(address(nctFlip), batchId, 0.8 ether, rad(200 ether));
+        user2.doDecreaseSoldAmount(address(nctFlip), batchId, 0.7 ether, rad(200 ether));
+        hevm.warp(nctFlip.totalAuctionLength() - 1);
+        user1.doDecreaseSoldAmount(address(nctFlip), batchId, 0.6 ether, rad(200 ether));
+        hevm.warp(now + nctFlip.totalAuctionLength() + 1);
+        user1.doSettleAuction(address(nctFlip), batchId);
+    }
+}
